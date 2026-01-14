7cf7c80719c126bd404451b2a3e3c31715c73a58
Bilal Nazeer <bilaldata50@gmail.com>
2026-01-14 23:12:00 +0500

    Commit Message

---
+++ app/src/main/java/com/example/facebook/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.facebook
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val etEmail = findViewById<EditText>(R.id.etEmail)
+        val etPassword = findViewById<EditText>(R.id.etPassword)
+        val btnLogin = findViewById<Button>(R.id.btnLogin)
+
+        btnLogin.setOnClickListener {
+            val email = etEmail.text.toString().trim()
+            val password = etPassword.text.toString().trim()
+
+            if (email.isEmpty() || password.isEmpty()) {
+                Toast.makeText(this, "Please fill all fields", Toast.LENGTH_SHORT).show()
+            } else {
+                val intent = Intent(this, WelcomeActivity::class.java)
+                intent.putExtra("USERNAME", email)
+                startActivity(intent)
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/facebook/WelcomeActivity.kt
@@ -0,0 +1,19 @@
+package com.example.facebook
+
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class WelcomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+
+        val tvWelcome = findViewById<TextView>(R.id.tvWelcome)
+        val username = intent.getStringExtra("USERNAME")
+        tvWelcome.text = "Welcome, $username!"
+
+
+    }
+}
